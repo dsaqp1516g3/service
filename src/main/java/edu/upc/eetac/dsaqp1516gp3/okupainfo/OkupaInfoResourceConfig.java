0c4bc68e4cb11f2c88d9ba75332e23baca06dd63
idoctnef <idoctnef@openmailbox.org>
2016-04-02 14:24:32 +0200

    punt 5.1 fet, filtre para autorizacion de peticiones fet

--- src/main/java/edu/upc/eetac/dsaqp1516gp3/okupainfo/OkupaInfoResourceConfig.java
+++ src/main/java/edu/upc/eetac/dsaqp1516gp3/okupainfo/OkupaInfoResourceConfig.java
@@ -1,4 +1,12 @@
 package edu.upc.eetac.dsaqp1516gp3.okupainfo;
 
-public class OkupaInfoResourceConfig {
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
+
+public class OkupaInfoResourceConfig extends ResourceConfig {
+    public OkupaInfoResourceConfig() {
+        packages("edu.upc.eetac.dsaqp1516gp3.okupainfo");
+        packages("edu.upc.eetac.dsaqp1516gp3.okupainfo.auth");
+        register(RolesAllowedDynamicFeature.class);
+    }
 }
