65e70a540f533376d5097f13409d6ebc8d8b8b15
idoctnef <idoctnef@openmailbox.org>
2016-04-02 14:41:28 +0200

    DAO de tokens de autorizacion fet, falta repassar les sql queries, és el punt 4.2fet

--- src/main/java/edu/upc/eetac/dsaqp1516gp3/okupainfo/dao/AuthTokenDAO.java
+++ src/main/java/edu/upc/eetac/dsaqp1516gp3/okupainfo/dao/AuthTokenDAO.java
@@ -1,4 +1,12 @@
 package edu.upc.eetac.dsaqp1516gp3.okupainfo.dao;
 
+import edu.upc.eetac.dsaqp1516gp3.okupainfo.auth.UserInfo;
+import edu.upc.eetac.dsaqp1516gp3.okupainfo.entity.AuthToken;
+
+import java.sql.SQLException;
+
 public interface AuthTokenDAO    {
+    public UserInfo getUserByAuthToken(String token) throws SQLException;
+    public AuthToken createAuthToken(String userid) throws SQLException;
+    public void deleteToken(String userid) throws SQLException;
 }
