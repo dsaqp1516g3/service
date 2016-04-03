7da2df300ba55327b79a447389af84ec401cb973
Max <max9@openmailbox.org>
2016-04-03 19:07:20 +0200

    Vacio

--- src/main/java/edu/upc/eetac/dsaqp1516gp3/okupainfo/dao/EventoDAOQuery.java
+++ src/main/java/edu/upc/eetac/dsaqp1516gp3/okupainfo/dao/EventoDAOQuery.java
@@ -13,11 +13,3 @@ public interface EventoDAOQuery
     String GET_ALL_EVENTS = "select *from events";
     String DELETE_EVENT = "delete from events where id=unhex(?)";
 }
-/*
-public final static String GET_USER_BY_TOKEN = "select hex(u.id) as id from users u, auth_tokens t where t.token=unhex(?) and u.id=t.userid";
-    public final static String UUID = "select REPLACE(UUID(),'-','')";
-    public final static String CREATE_TOKEN = "insert into auth_tokens (userid, token) values (UNHEX(?), UNHEX(?))";
-    public final static String GET_USER_BY_TOKEN = "select hex(u.id) as id from users u, auth_tokens t where t.token=unhex(?) and u.id=t.userid";
-    public final static String GET_ROLES_OF_USER = "select hex(userid), role from user_roles where userid=unhex(?)";
-    public final static String DELETE_TOKEN = "delete from auth_tokens where userid = unhex(?)";
- */
