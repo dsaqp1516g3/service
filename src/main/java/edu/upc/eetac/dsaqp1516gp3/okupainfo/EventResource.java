dc0ee707f6b26d5bb6e1b0d758c943e87d9cde38
Max <max9@openmailbox.org>
2016-04-21 16:07:35 +0200

    EventResource

--- src/main/java/edu/upc/eetac/dsaqp1516gp3/okupainfo/EventResource.java
+++ src/main/java/edu/upc/eetac/dsaqp1516gp3/okupainfo/EventResource.java
@@ -20,17 +20,17 @@ public class EventResource
     @POST
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(OkupaInfoMediaType.OKUPAINFO_AUTH_TOKEN)
-    public Response createEvent(@FormParam("casalid") String casalid, @FormParam("title") String title, @FormParam("description") String description, @FormParam("localization") String localization, @FormParam("latitude") Double latitude, @FormParam("longitude") Double longitude, @Context UriInfo uriInfo) throws URISyntaxException
+    public Response createEvent(@FormParam("casalid") String casalid, @FormParam("title") String title, @FormParam("description") String description, @FormParam("localization") String localization, @FormParam("latitude") double latitude, @FormParam("longitude") double longitude, @FormParam("eventdate") double eventdate, @Context UriInfo uriInfo) throws URISyntaxException
     {
-        if (casalid == null || title == null || description == null || localization == null || latitude == null || longitude == null)
+        if (casalid == null || title == null || description == null || localization == null || latitude == 0 || longitude == 0 || eventdate == 0)
             throw new BadRequestException("all parameters are mandatory");
         EventoDAO EventoDAO = new EventoDAOImpl();
         Event Event;
         AuthToken authenticationToken;
         try
         {
-/*Aqui atacamos a la API externa para obtener las longitudes y latitudes*/
-            Event = EventoDAO.createEvent(casalid, title, description, localization, latitude, longitude);
+            /*Aqui atacamos a la API externa para obtener las longitudes y latitudes*/
+            Event = EventoDAO.createEvent(casalid, title, description, localization, latitude, longitude, eventdate);
             authenticationToken = (new AuthTokenDAOImpl()).createAuthToken(Event.getId());
         }
         catch (SQLException e)
@@ -39,6 +39,58 @@ public class EventResource
         }
         URI uri = new URI(uriInfo.getAbsolutePath().toString() + "/" + Event.getId());
         return Response.created(uri).type(OkupaInfoMediaType.OKUPAINFO_AUTH_TOKEN).entity(authenticationToken).build();
+    }
+
+    @Path("/{id}")
+    @PUT
+    @Consumes(OkupaInfoMediaType.OKUPAINFO_EVENTS)
+    @Produces(OkupaInfoMediaType.OKUPAINFO_EVENTS)
+    public Event updateProfile(@PathParam("id") String id, @PathParam("title") String title, @PathParam("description") String description, @PathParam("eventdate") double eventdate, Event event) {
+        if (event == null)
+            throw new BadRequestException("entity is null");
+        if (!id.equals(event.getId()))
+            throw new BadRequestException("path parameter id and entity parameter id doesn't match");
+        String userid = securityContext.getUserPrincipal().getName();
+        if(!userid.equals(event.getCasalid()))
+            throw new ForbiddenException("operation not allowed");
+        EventoDAO EventoDAO = new EventoDAOImpl();
+        try
+        {
+            event = EventoDAO.updateProfile(id, title, description, eventdate);
+            if (event == null)
+                throw new NotFoundException("El evento con la id " + id + " no existe");
+        }
+        catch (SQLException e)
+        {
+            throw new InternalServerErrorException();
+        }
+        return event;
+    }
+
+    @Path("/{id}")
+    @PUT
+    @Consumes(OkupaInfoMediaType.OKUPAINFO_EVENTS)
+    @Produces(OkupaInfoMediaType.OKUPAINFO_EVENTS)
+    public Event updateLocation(@PathParam("id") String id, @PathParam("localization") String localization, @PathParam("latitude") double latitude, @PathParam("longitude") double longitude, Event event) {
+        if (event == null)
+            throw new BadRequestException("entity is null");
+        if (!id.equals(event.getId()))
+            throw new BadRequestException("path parameter id and entity parameter id doesn't match");
+        String userid = securityContext.getUserPrincipal().getName();
+        if(!userid.equals(event.getCasalid()))
+            throw new ForbiddenException("operation not allowed");
+        EventoDAO eventoDAO = new EventoDAOImpl();
+        try
+        {
+            event = eventoDAO.updateLocation(id, localization, latitude, longitude);
+            if (event == null)
+                throw new NotFoundException("El evento con la id " + id + " no existe");
+        }
+        catch (SQLException e)
+        {
+            throw new InternalServerErrorException();
+        }
+        return event;
     }
 
     @Path("/{id}")
@@ -62,53 +114,6 @@ public class EventResource
 
     @Path("/{id}")
     @GET
-    @Produces(OkupaInfoMediaType.OKUPAINFO_EVENTS)//Miramos la asistencia de un usuario a varios eventos
-    public Event getEventsByUserId(@PathParam("userid") String userid,@QueryParam("timestamp") long timestamp, @DefaultValue("true") @QueryParam("before") boolean before, @Context Request request)
-    {
-        /*CacheControl cacheControl = new CacheControl();
-        EventoDAO eventoDAO = new EventoDAOImpl();
-        EventCollection eventCollection;
-        Event event;
-
-        try
-        {
-            if (before && timestamp == 0) timestamp = System.currentTimeMillis();
-            eventCollection = eventoDAO.getAllEvents(timestamp, before);
-            event = eventoDAO.getEventsByUserId(userid, timestamp,before);
-            if (event == null)
-                throw new NotFoundException("Event with userid = " + userid + " doesn't exist");
-
-            // Calculate the ETag on last modified date of user resource
-            EntityTag eTag = new EntityTag(Long.toString(event.getLastModified()));
-
-            // Verify if it matched with etag available in http request
-            Response.ResponseBuilder rb = request.evaluatePreconditions(eTag);
-
-            // If ETag matches the rb will be non-null;
-            // Use the rb to return the response without any further processing
-            if (rb != null) {
-                return rb.cacheControl(cacheControl).tag(eTag).build();
-            }
-
-            // If rb is null then either it is first time request; or resource is
-            // modified
-            // Get the updated representation and return with Etag attached to it
-            rb = Response.ok(event).cacheControl(cacheControl).tag(eTag);
-            return rb.build();
-        }
-        catch (SQLException e)
-        {
-            throw new InternalServerErrorException(e.getMessage());
-        }
-        if (Event == null)
-            throw new NotFoundException("Event with userid = " + userid + "doesn't exist");
-        return Event;*/
-
-        return null;
-    }
-
-    @Path("/{id}")
-    @GET
     @Produces(OkupaInfoMediaType.OKUPAINFO_EVENTS_COLLECTION)
     public EventCollection getEventsByCreatorId(@PathParam("userid") String userid,@QueryParam("timestamp") long timestamp, @DefaultValue("true") @QueryParam("before") boolean before)
     {
@@ -126,74 +131,12 @@ public class EventResource
         return eventCollection;
     }
 
-
     @Path("/{id}")
-    @PUT
-    @Consumes(OkupaInfoMediaType.OKUPAINFO_EVENTS)
-    @Produces(OkupaInfoMediaType.OKUPAINFO_EVENTS) //No coge bien los parametros pasados?????
-    public Event updateProfile(@PathParam("id") String id, @PathParam("title") String title, @PathParam("description") String description, Event event) {
-        if (event == null)
-            throw new BadRequestException("entity is null");
-        if (!id.equals(event.getId()))
-            throw new BadRequestException("path parameter id and entity parameter id doesn't match");
-        String userid = securityContext.getUserPrincipal().getName();
-        if(!userid.equals(event.getCasalid()))
-            throw new ForbiddenException("operation not allowed");
-        EventoDAO EventoDAO = new EventoDAOImpl();
-        try
-        {
-            event = EventoDAO.updateProfile(id, title, description);
-            if (event == null)
-                throw new NotFoundException("El evento con la id " + id + " no existe");
-        }
-        catch (SQLException e)
-        {
-            throw new InternalServerErrorException();
-        }
-        return event;
-    }
-
-    @Path("/{id}")
-    @PUT
-    @Consumes(OkupaInfoMediaType.OKUPAINFO_EVENTS)
-    @Produces(OkupaInfoMediaType.OKUPAINFO_EVENTS)//No coge bien los parametros pasados?????
-    public Event updateLocation(@PathParam("id") String id, @PathParam("localization") String localization, @PathParam("latitude") double latitude, @PathParam("longitude") double longitude, Event event) {
-        if (event == null)
-            throw new BadRequestException("entity is null");
-        if (!id.equals(event.getId()))
-            throw new BadRequestException("path parameter id and entity parameter id doesn't match");
-        String userid = securityContext.getUserPrincipal().getName();
-        if(!userid.equals(event.getCasalid()))
-            throw new ForbiddenException("operation not allowed");
-        EventoDAO eventoDAO = new EventoDAOImpl();
-        try
-        {
-            event = eventoDAO.updateLocation(id, localization, latitude, longitude);
-            if (event == null)
-                throw new NotFoundException("El evento con la id " + id + " no existe");
-        }
-        catch (SQLException e)
-        {
-            throw new InternalServerErrorException();
-        }
-        return event;
-    }
-
-    @Path("({id}")
-    @RolesAllowed("[admin, casal]")
-    @DELETE
-    public void deleteEvent(@PathParam("id") String id)
+    @GET
+    @Produces(OkupaInfoMediaType.OKUPAINFO_EVENTS)//Miramos la asistencia de un usuario a varios eventos
+    public Event getEventsByUserId(@PathParam("userid") String userid,@QueryParam("timestamp") long timestamp, @DefaultValue("true") @QueryParam("before") boolean before, @Context Request request)
     {
-        EventoDAO EventoDAO = new EventoDAOImpl();
-        try
-        {
-            if (!EventoDAO.deleteEvent(id))
-                throw new NotFoundException("Casal with id = " + id + " doesn't exist");
-        }
-        catch (SQLException e)
-        {
-            throw new InternalServerErrorException();
-        }
+        return null;
     }
 
     @GET
@@ -212,5 +155,22 @@ public class EventResource
             throw new InternalServerErrorException();
         }
         return EventCollection;
+    }
+
+    @Path("({id}")
+    @RolesAllowed("[admin, casal]")
+    @DELETE
+    public void deleteEvent(@PathParam("id") String id)
+    {
+        EventoDAO EventoDAO = new EventoDAOImpl();
+        try
+        {
+            if (!EventoDAO.deleteEvent(id))
+                throw new NotFoundException("Casal with id = " + id + " doesn't exist");
+        }
+        catch (SQLException e)
+        {
+            throw new InternalServerErrorException();
+        }
     }
 }
