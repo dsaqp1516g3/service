0f9c39b1e06aa90dde00d40145e4d6617d410cfe
Guillermo <guillermo.riverola@outlook.com>
2016-05-21 00:16:16 +0200

    @InjectLinks

--- src/main/java/edu/upc/eetac/dsaqp1516gp3/okupainfo/entity/User.java
+++ src/main/java/edu/upc/eetac/dsaqp1516gp3/okupainfo/entity/User.java
@@ -1,6 +1,12 @@
 package edu.upc.eetac.dsaqp1516gp3.okupainfo.entity;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import edu.upc.eetac.dsaqp1516gp3.okupainfo.LoginResource;
+import edu.upc.eetac.dsaqp1516gp3.okupainfo.OkupaInfoMediaType;
+import edu.upc.eetac.dsaqp1516gp3.okupainfo.OkupaInfoRootAPIResource;
+import edu.upc.eetac.dsaqp1516gp3.okupainfo.UserResource;
+import org.glassfish.jersey.linking.Binding;
+import org.glassfish.jersey.linking.InjectLink;
 import org.glassfish.jersey.linking.InjectLinks;
 
 import javax.ws.rs.core.Link;
@@ -9,7 +15,11 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class User
 {
-    @InjectLinks({})
+    @InjectLinks({
+            @InjectLink(resource = OkupaInfoRootAPIResource.class, style = InjectLink.Style.ABSOLUTE, rel = "home", title = "Rate Root API"),
+            @InjectLink(resource = LoginResource.class, style = InjectLink.Style.ABSOLUTE, rel = "logout", title = "Logout"),
+            @InjectLink(resource = UserResource.class, method = "getUser", style = InjectLink.Style.ABSOLUTE, rel = "self user-profile", title = "User profile", type= OkupaInfoMediaType.OKUPAINFO_USER, bindings = @Binding(name = "id", value = "${instance.id}"))
+    })
     private List<Link> links;
     private String id;
     private String loginid;
