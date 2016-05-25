4553a6ce965273a02423340c773ba08c086965ed
lima9 <max9@openmailbox.org>
2016-05-25 17:14:11 +0200

    HATEOS de casals, users y events

--- src/main/java/edu/upc/eetac/dsaqp1516gp3/okupainfo/entity/Event.java
+++ src/main/java/edu/upc/eetac/dsaqp1516gp3/okupainfo/entity/Event.java
@@ -1,13 +1,24 @@
 package edu.upc.eetac.dsaqp1516gp3.okupainfo.entity;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import edu.upc.eetac.dsaqp1516gp3.okupainfo.CasalResource;
+import edu.upc.eetac.dsaqp1516gp3.okupainfo.EventResource;
+import edu.upc.eetac.dsaqp1516gp3.okupainfo.OkupaInfoMediaType;
+import edu.upc.eetac.dsaqp1516gp3.okupainfo.OkupaInfoRootAPIResource;
+import org.glassfish.jersey.linking.InjectLink;
+import org.glassfish.jersey.linking.InjectLinks;
 
 import javax.ws.rs.core.Link;
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class Event
-{
+public class Event {
+
+    @InjectLinks({
+            @InjectLink(resource = OkupaInfoRootAPIResource.class, style = InjectLink.Style.ABSOLUTE, rel = "home", title = "OkupaInfo Root API"),
+            @InjectLink(resource = EventResource.class, style = InjectLink.Style.ABSOLUTE, rel = "current-events", title = "Current Events", type = OkupaInfoMediaType.OKUPAINFO_EVENTS_COLLECTION),
+            @InjectLink(resource = CasalResource.class, style = InjectLink.Style.ABSOLUTE, rel = "current-casals", title = "Current Casals", type = OkupaInfoMediaType.OKUPAINFO_CASAL_COLLECTION),
+    })
     private List<Link> links;
     private String id;
     private String casalid;
--- src/main/java/edu/upc/eetac/dsaqp1516gp3/okupainfo/entity/User.java
+++ src/main/java/edu/upc/eetac/dsaqp1516gp3/okupainfo/entity/User.java
@@ -1,13 +1,23 @@
 package edu.upc.eetac.dsaqp1516gp3.okupainfo.entity;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import edu.upc.eetac.dsaqp1516gp3.okupainfo.*;
+import org.glassfish.jersey.linking.Binding;
+import org.glassfish.jersey.linking.InjectLink;
+import org.glassfish.jersey.linking.InjectLinks;
 
 import javax.ws.rs.core.Link;
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class User
-{
+public class User {
+    @InjectLinks({
+            @InjectLink(resource = OkupaInfoRootAPIResource.class, style = InjectLink.Style.ABSOLUTE, rel = "home", title = "OkupaInfo Root API"),
+            @InjectLink(resource = CasalResource.class, style = InjectLink.Style.ABSOLUTE, rel = "current-casals", title = "Current casals", type = OkupaInfoMediaType.OKUPAINFO_CASAL_COLLECTION),
+            @InjectLink(resource = LoginResource.class, style = InjectLink.Style.ABSOLUTE, rel = "logout", title = "Logout"),
+            @InjectLink(resource = EventResource.class, style = InjectLink.Style.ABSOLUTE, rel = "create-event", title = "Create Event", type = OkupaInfoMediaType.OKUPAINFO_EVENTS),
+            @InjectLink(resource = UserResource.class, method = "getUser", style = InjectLink.Style.ABSOLUTE, rel = "self user-profile", title = "User profile", type = OkupaInfoMediaType.OKUPAINFO_USER, bindings = @Binding(name = "id", value = "${instance.id}"))
+    })
     private List<Link> links;
     private String id;
     private String loginid;
